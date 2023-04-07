6629939afcb1982b9ec0e5e24d5b274e40deeb57
Salapoi <94974750+Daniel-Esteban-Lahti-NTI-Johanneberg@users.noreply.github.com>
2023-04-07 20:05:24 +0200

    Yardoc

--- Daily-Doodle/app.rb
+++ Daily-Doodle/app.rb
@@ -8,9 +8,44 @@ require_relative './model.rb'
 include Model
 
 enable :sessions
-db = SQLite3::Database.new('db/DailyDoodleDatabase.db')
+db = SQLite3::Database.new('Daily-Doodle/db/DailyDoodleDatabase.db')
 db.results_as_hash = true
 
+helpers do
+  # Helper function to access the database
+  #
+  def getDB()
+      db = SQLite3::Database.new('Daily-Doodle/db/DailyDoodleDatabase.db')
+      db.results_as_hash = true
+      return db
+  end
+
+  # Helper function to determine if a user is already following a certain user
+  #
+  # @param [Integer] followed_user_id, The id of the user being followed
+  # @param [Integer] following_user_id, The id of the user following
+  def user_is_following(followed_user_id,following_user_id)
+      db = getDB()
+      return db.execute('SELECT * FROM follows_rel WHERE followed_user_id = ? AND following_user_id = ?',followed_user_id,following_user_id).length != 0
+  end
+
+  # Helper function to determine if a user has already like a certain doodle
+  #
+  # @param [Integer] doodle_id, The id of the liked doodle
+  # @param [Integer] user_id, The id of the liking user
+  def user_has_liked(doodle_id,user_id)
+      db = getDB()
+      return db.execute('SELECT * FROM likes_rel WHERE doodle_id = ? AND user_id = ?',doodle_id,user_id).length != 0
+  end
+end
+
+# Displays landing page, if user is logged in and has already posted today, displays feed
+#
+# @param [Boolean] session[:loggedin], Stored session if a user is logged in
+# @param [String] session[:prompt], Stored session of the current prompt of the day
+# @param [Array] doodles, List of hashes of all the doodles posted today
+#
+# @see Model#user_has_posted
 get('/') do
   session[:prompt] = db.execute('SELECT prompt_name FROM prompts WHERE current_prompt = 1').first[0].to_s
   if user_has_posted(session[:id],session[:prompt]) && session[:loggedin] == true
@@ -21,20 +56,27 @@ get('/') do
   end
 end
 
-#Eftersom det inte finns bools i SQLite använder jag integersen (1,0) som (true,false)
+# Updates the current prompt of the day (Note: 1 and 0 are used instead of true and false as booleans aren't inherent in SQLite)
+#
+# @see Model#update_prompt
 post('/prompts/update') do
-  #update_current_prompt()
-  current_prompt_id = db.execute('SELECT prompt_id FROM prompts WHERE current_prompt = 1').first[0]
-  new_prompt_id = db.execute('SELECT prompt_id FROM prompts ORDER BY RANDOM() LIMIT 1').first[0]
-  db.execute('UPDATE prompts SET current_prompt = 0 WHERE prompt_id = ?',current_prompt_id)
-  db.execute('UPDATE prompts SET current_prompt = 1 WHERE prompt_id = ?',new_prompt_id)
-  redirect('/')
+  update_prompt()
 end
 
-get('/showlogin') do
+# Displays a login form
+#
+get('/login') do
   slim(:login)
 end
 
+# Attempts login and updates the session(s) 
+#
+# @param [String] username, The username
+# @param [String] password, The password
+#
+# @param [Integer] session[:id], The id of the user
+# @param [String] session[:id], The username of the user
+# @param [Boolean] session[:loogedin], Whether or not user is logged in (used for safety in routes)
 post('/login') do
   username = params[:username]
   password = params[:password]
@@ -52,38 +94,60 @@ post('/login') do
   end
 end
 
+# Logs out and clears user-specific sessions
+#
+# @see Model#log_out
 post('/logout') do
   log_out()
 end
 
+# Displays a form to register a new account
+#
 get('/users/new') do
   slim(:register)
 end
 
+# Displays a user's profile page
+# 
+# @param [String] :username, The username of the profile's user
 get('/users/:username') do
-  slim(:profile)
+  username = params[:username].to_s
+  doodles = db.execute('SELECT * FROM doodles WHERE user_id = (SELECT user_id FROM users WHERE username = ?)',username)
+  friends = db.execute('SELECT * FROM follows_rel WHERE following_user_id = (SELECT user_id FROM users WHERE username = ?)',username)
+  slim(:profile, locals:{doodles:doodles,friends:friends,username:username})
 end
 
-post('/users') do #ta bort new
+# Attempts to register a new user and adds their data to the database
+#
+# @param [String] username, The username
+# @param [String] password, The password
+# @param [String] repeat-password, The repeated password
+#
+# @see Model#register
+post('/users') do
   username = params[:username]
   password = params[:password]
   password_confirm = params[:password_confirm]
   register(username,password,password_confirm)
 end
 
+# Displays a form to upload a doodle
+#
 get('/doodles/new') do
   slim(:upload)
 end
 
+# Validates if the inputted file is an image, then saves the image as a file on the local disc and as a URL in the database
+# 
+# @param [String] image, The image name
+# @param [String] path, The image filepath including the image name and extension
 post('/doodles') do
-  # Kolla ifall det är en bild usern har uploadat
   if params[:image] && params[:image][:filename]
     filename = params[:image][:filename]
     file = params[:image][:tempfile]
     dbPath = "/doodles/#{filename}" #/#{session[:prompt]}
-    path = "./public/doodles/#{filename}"
+    path = "Daily-Doodle/public/doodles/#{filename}"
     db.execute('INSERT into doodles (user_id,url,prompt) VALUES (?,?,?)',session[:id],dbPath,session[:prompt])
-    # Spara filen på lokal
     File.open(path, 'wb') do |f|
       f.write(file.read)
     end
@@ -91,52 +155,81 @@ post('/doodles') do
     end
 end
 
+# Creates a new follow (many to many relationship) between two users
+#
+# @param [Integer] followed_user_id, The id of the user being followed
+# @param [Integer] user_id, The id of the user following
+#
+# @see Model#follow
 post('/follows') do
   followed_user_id = params[:followed_user_id]
   user_id = session[:id]
   follow(followed_user_id,user_id)
 end
 
+# Deletes a follow (many to many relationship)
+#
+# @param [Integer] followed_user_id, The id of the user being followed
+# @param [Integer] user_id, The id of the user following
+#
+# @see Model#unfollow
 post('/follows/delete') do
   followed_user_id = params[:followed_user_id]
   following_user_id = session[:id]
   unfollow(followed_user_id,following_user_id)
 end
 
+# Creates a new like (many to many relationship) between a user and a doodle 
+#
+# @param [Integer] doodle_id, The id of the liked doodle
+# @param [Integer] user_id, The id of the liking user
+#
+# @see Model#like
 post('/doodles/:doodle_id/like') do
   doodle_id = params[:doodle_id]
   user_id = session[:id]
   like(doodle_id,user_id)
 end
 
+# Deletes like (many to many relationship) 
+#
+# @param [Integer] doodle_id, The id of the liked doodle
+# @param [Integer] user_id, The id of the liking user
+#
+# @see Model#unlike
 post('/doodles/:doodle_id/unlike') do
   doodle_id = params[:doodle_id]
   user_id = session[:id]
   unlike(doodle_id,user_id)
 end
 
+# Deletes a doodle
+#
+# @param [Integer] :doodle_id, The id of the doodle
+# @param [Integer] user_id, The id of the user attempting to delete the doodle (used to evaluate the user's eligibility of deleting the doodle)
+#
+# @see Model#delete_doodle
 post('/doodles/:doodle_id/delete') do
   doodle_id = params[:doodle_id].to_i
   user_id = session[:id]
   delete_doodle(doodle_id,user_id)
 end
 
+# Reports a doodle
+#
+# @param [Integer] :doodle_id, The id of the reported doodle
 post('/doodles/:doodle_id/report') do
   doodle_id = params[:doodle_id]
-
   redirect('/')
 end
 
-get('/feed') do
-  slim(:feed)
-end
-
+# Route for debugging countdown timer and current prompt of the day
+#
+# @param [String] session[:prompt], Current prompt of the day
 get('/countdown') do
   session[:prompt] = db.execute('SELECT prompt_name FROM prompts WHERE current_prompt = 1').first[0].to_s
   slim(:prompt)
 end
-
-
 
 #kunna ta bort allt en användare har (typ id vid flera tabeller)
 #On-delete cascade
--- Daily-Doodle/model.rb
+++ Daily-Doodle/model.rb
@@ -4,24 +4,27 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 module Model
-    helpers do
-        def getDB()
-            db = SQLite3::Database.new('db/DailyDoodleDatabase.db')
-            db.results_as_hash = true
-            return db
-        end
-
-        def user_is_following(followed_user_id,following_user_id)
-            db = getDB()
-            return db.execute('SELECT * FROM follows_rel WHERE followed_user_id = ? AND following_user_id = ?',followed_user_id,following_user_id).length != 0
-        end
-
-        def user_has_liked(doodle_id,user_id) #session[:id] som parameter i funktionen
-            db = getDB()
-            return db.execute('SELECT * FROM likes_rel WHERE doodle_id = ? AND user_id = ?',doodle_id,user_id).length != 0
-        end
+    def get_DB()
+        db = SQLite3::Database.new('Daily-Doodle/db/DailyDoodleDatabase.db')
+        db.results_as_hash = true
+        return db
+    end
+    # Updates the current prompt of the day by changing the 'current_prompt' column
+    #
+    def update_prompt()
+        db = get_DB()
+        current_prompt_id = db.execute('SELECT prompt_id FROM prompts WHERE current_prompt = 1').first[0]
+        new_prompt_id = db.execute('SELECT prompt_id FROM prompts ORDER BY RANDOM() LIMIT 1').first[0]
+        db.execute('UPDATE prompts SET current_prompt = 0 WHERE prompt_id = ?',current_prompt_id)
+        db.execute('UPDATE prompts SET current_prompt = 1 WHERE prompt_id = ?',new_prompt_id)
+        redirect('/')
     end
 
+    # Clears all user-related sessions
+    #
+    # @params [Integer] session[:id], The stored id of the current user
+    # @params [String] session[:username], The stored username of the current user
+    # @params [Boolean] session[:loggedin], Global bool used to determine if someone is logged in
     def log_out()
         session[:id] = nil
         session[:username] = nil
@@ -29,8 +32,13 @@ module Model
         redirect('/')
     end
 
+    # Creates a new user in the 'users' table
+    #
+    # @param [String] username, The username
+    # @param [String] password, The password (Password is stored as password digest in database)
+    # @param [String] password-confirm, Repeated password for confirmation (Password is stored as password digest in database)
     def register(username,password,password_confirm)
-        db = getDB()
+        db = get_DB()
         if password == password_confirm
             password_digest = BCrypt::Password.create(password)
             db.execute('INSERT INTO users (username,password_digest) VALUES (?,?)',username,password_digest)
@@ -38,38 +46,62 @@ module Model
         end  
     end
 
+    # Determines if a certain user has posted a doodle with today's prompt
+    #
+    # @param [Integer] user_id, The id of the user
+    # @param [String] prompt, The current prompt of the day
     def user_has_posted(user_id,prompt)
-        db = getDB()
+        db = get_DB()
         p db.execute('SELECT * FROM doodles WHERE user_id = ? AND prompt = ?',user_id,prompt)
         return db.execute('SELECT * FROM doodles WHERE user_id = ? AND prompt = ?',user_id,prompt).length != 0
     end
 
+    # Creates a follow (many to many relationship) between two users
+    #
+    # @param [Integer] followed_user_id, The id of the user being followed
+    # @param [Integer] user_id, The id of the user following
     def follow(followed_user_id,user_id)
-        db = getDB()
+        db = get_DB()
         db.execute('INSERT INTO follows_rel (following_user_id,followed_user_id) VALUES (?,?)',user_id,followed_user_id)
         redirect('/')
     end
 
+    # Deletes a follow (many to many relationship) between two users
+    #
+    # @param [Integer] followed_user_id, The id of the user being followed
+    # @param [Integer] user_id, The id of the user following
     def unfollow(followed_user_id,following_user_id)
-        db = getDB()
+        db = get_DB()
         db.execute('DELETE FROM follows_rel WHERE following_user_id = ? AND followed_user_id = ?',following_user_id,followed_user_id)
         redirect('/')
     end
 
+    # Creates a like (many to many relationship) between a user and a doodle
+    #
+    # @param [Integer] doodle_id, The id of the liked doodle
+    # @param [Integer] user_id, The id of the liking user
     def like(doodle_id,user_id)
-        db = getDB()
+        db = get_DB()
         db.execute('INSERT INTO likes_rel (user_id,doodle_id) VALUES (?,?)',user_id,doodle_id)
         redirect('/')
     end
-
+   
+    # Deletes a like (many to many relationship) between a user and a doodle
+    #
+    # @param [Integer] doodle_id, The id of the liked doodle
+    # @param [Integer] user_id, The id of the liking user
     def unlike(doodle_id,user_id)
-        db = getDB()
+        db = get_DB()
         db.execute('DELETE FROM likes_rel WHERE user_id = ? AND doodle_id = ?',user_id,doodle_id)
         redirect('/')
     end
 
+    # Checks if the user is eligible for deleting a certain post and then deletes it
+    #
+    # @param [Integer] doodle_id, The id of the doodle being deleted
+    # @param [Integer] user_id, The id of the user attempting to delete the doodle
     def delete_doodle(doodle_id,user_id)
-        db = getDB()
+        db = get_DB()
         if db.execute('SELECT user_id FROM doodles WHERE doodle_id = ?',doodle_id).first[0] == (user_id || 1) #user_id 1 = admin
             db.execute('DELETE FROM doodles WHERE doodle_id = ?',doodle_id)
         end
